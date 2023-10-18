ab74a78014fea7cfeb8337dff91c06a22125e856
JuanCamiloYepesAlbarracin <jyepes511@unab.edu.co>
2023-10-18 15:54:31 -0500

    Feat homepage 001

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,15 @@
-import { Navbar } from "../components";
+import { NavBarHome } from "../components";
 
 export const HomePage = () => {
   return (
     <>
-      <Navbar />
-      <h1>hola</h1>
+    <NavBarHome />
+    <img src="https://www.zonafrancasantander.com/imagenes/vdo_cabezotes/cabe_93d666c2e538322a27e6562c19bc5597a2066bd6.jpg" className="contenido img-fluid"/>
+    <img
+          src="https://i.imgur.com/9PDGhjx.jpg"
+          className="contenido-alternativa img-fluid contenido "
+        />
+    
     </>
   );
 };
