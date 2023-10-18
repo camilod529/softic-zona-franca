dde04c445ed091b416de218f421302e34262dc84
JuanCamiloYepesAlbarracin <jyepes511@unab.edu.co>
2023-10-18 05:51:51 -0500

    feat event_list 001

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-
+import { Navbar, Adm_event } from "./components";
 import { App } from "./App";
 import { store } from "./store";
 import Footer from "./components/Footer";
@@ -11,6 +11,8 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
+      <Navbar/>
+      <Adm_event/>
         <App />
         <Footer/>
       </BrowserRouter>
