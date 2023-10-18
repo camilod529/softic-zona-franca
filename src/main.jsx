055857649dea5b173ac820737d070fde0520d7bd
JuanCamiloYepesAlbarracin <jyepes511@unab.edu.co>
2023-10-18 09:07:32 -0500

    .

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { Navbar, AdmEmp } from "./components";
 import { App } from "./App";
 import { store } from "./store";
 import Footer from "./components/Footer";
