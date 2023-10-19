721aa62f2a7c7e9f227cf462893126fc64a8539d
JuanCamiloYepesAlbarracin <jyepes511@unab.edu.co>
2023-10-19 08:02:36 -0500

    AmdEmpAge fix view

--- src/components/CompCard.tsx
+++ src/components/CompCard.tsx
@@ -1,16 +1,30 @@
+import { Link } from "react-router-dom";
+
 export const CompCard =() =>{
     return(
         <>
-
-<div className="col">
-    <div className="card h-100">
-      <img src="" className="card-img-top" alt="..."/>
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+<div className="card" style={{ width: "18rem" }}>
+        <img src="..." className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Nombre de empresa</h5>
+          <p className="card-text">
+            
+          </p>
+        </div>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">NIT</li>
+          <li className="list-group-item">
+            <Link to="#" className="card-link">
+              Ubicacion
+            </Link>
+          </li>
+          <li className="list-group-item">
+            <Link to="#" className="card-link">
+              Pagina web
+            </Link>
+          </li>
+        </ul>
       </div>
-    </div>
-  </div>
-        </>
+    </>
     );
 };
