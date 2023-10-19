6f37d9c9024e4c5b49cb05074edd48bbe4fc8e1f
DFDev3 <ddiaz216@unab.edu.co>
2023-10-19 01:07:52 -0500

    TagsPage FrontEnd

--- src/pages/TagsPage.tsx
+++ src/pages/TagsPage.tsx
@@ -8,6 +8,7 @@ export const TagsPage = () => {
       <Navbar />
       <h1 className="stylish-headline mt-5">Intereses</h1>
 
+      <div className="greaterdiv"></div>
       <div className="interests-container">
         <form id="interests-form">
           <Tag />
@@ -46,9 +47,13 @@ export const TagsPage = () => {
           <Tag />
           <Tag />
           <Tag />
-          <button type="button">Save Interests</button>
+<br /><br />
+          <button className="float-end marRight" type="button">
+            Save Interests
+          </button>
         </form>
       </div>
+
       <Footer />
     </>
   );
