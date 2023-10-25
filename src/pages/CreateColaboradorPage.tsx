d4b5ba421d0e06712ddf36599d34a42656069dae
JuanCamiloYepesAlbarracin <jyepes511@unab.edu.co>
2023-10-25 13:44:29 -0500

    Gender Lavel (waiting for fix conflicts )

--- src/pages/CreateColaboradorPage.tsx
+++ src/pages/CreateColaboradorPage.tsx
@@ -15,7 +15,7 @@ export const CreateColaboradorPage = () => {
     foto: null,
   });
   const navigate = useNavigate();
-
+  
   const onChange = (e: FormEvent<HTMLInputElement>) => {
     if (e.currentTarget.files) {
       setData({ ...data, [e.currentTarget.name]: e.currentTarget.files[0] });
@@ -44,109 +44,100 @@ export const CreateColaboradorPage = () => {
       .catch((err) => console.log(err));
   };
   return (
+    
     <>
       <Navbar />
-      
-  <div className="row justify-content-md-center">
-      <div className="col col-lg-2">
-    
-    </div>
-      <div className="col-md-auto">
-    <main>
-        <form className="colaborator-form mr-4" onSubmit={handleSubmit}>
-          <label htmlFor="documento_colaborador">Documento Colaborador</label>
-          <input
-            type="text"
-            name="documento_colaborador"
-            id="documento_colaborador"
-            placeholder="Escriba el documento del colaborador"
-            onChange={onChange}
-          />{" "}
-          <br />
-          <br />
-          <label htmlFor="empresa_colaborador">NIT empresa colaborador</label>
-          <input
-            type="text"
-            name="empresa_colaborador"
-            id="empresa_colaborador"
-            placeholder="Escriba el nit de la empresa del colaborador"
-            onChange={onChange}
-          />{" "}
-          <br />
-          <label htmlFor="nombre_1">Primer Nombre</label>
-          <input
-            type="text"
-            name="nombre_1"
-            id="nombre_1"
-            placeholder="Escriba el primer nombre del colaborador"
-            onChange={onChange}
-          />{" "}
-          <br />
-          <label htmlFor="apellido_1">Primer Apellido</label>
-          <input
-            type="text"
-            name="apellido_1"
-            id="apellido_1"
-            placeholder="Escriba el primer apellido del colaborador"
-            onChange={onChange}
-          />{" "}
-          <br />
-          <label htmlFor="genero">Genero</label>
-          <select aria-label="Seleccione el genero" >
-  <option selected>Seleccione el genero</option>
-  <option value="1">Masculino</option>
-  <option value="2">Femenino</option>
- 
-</select>
-          <input
-            type="text"
-            name="genero"
-            id="genero"
-            placeholder="Escriba el genero del colaborador"
-            onChange={onChange}
-          />{" "}
-          <br />
-          <label htmlFor="correo_personal">Correo personal</label>
-          <input
-            type="text"
-            name="correo_personal"
-            id="correo_personal"
-            placeholder="Escriba el correo personal del colaborador"
-            onChange={onChange}
-          />{" "}
-          <br />
-          <label htmlFor="fecha_nacimiento">Fecha nacimiento</label>
-          <input
-            type="datetime-local"
-            name="fecha_nacimiento"
-            id="fecha_nacimiento"
-            placeholder="Ingrese la fecha de nacimiento del colaborador"
-            onChange={onChange}
-          />{" "}
-          <br />
-          <label htmlFor="foto">Foto</label>
-          <input
-            type="file"
-            name="foto"
-            id="foto"
-            placeholder="Escriba el nit de la empresa del colaborador"
-            onChange={onChange}
-          />{" "}
-          <br />
-          <button className="btn btn-primary mt-3">Crear colaborador</button>
-        </form>
-      </main>
-    </div>
-    <div className="col col-lg-2">
-      
-    </div>
-    </div>
 
+      <div className="row justify-content-md-center">
+        <div className="col col-lg-2"></div>
+        <div className="col-md-auto">
+          <main>
+            <form className="colaborator-form mr-4" onSubmit={handleSubmit}>
+              <label htmlFor="documento_colaborador">
+                Documento Colaborador
+              </label>
+              <input
+                type="text"
+                name="documento_colaborador"
+                id="documento_colaborador"
+                placeholder="Escriba el documento del colaborador"
+                onChange={onChange}
+              />{" "}
+              <br />
+              <br />
+              <label htmlFor="empresa_colaborador">
+                NIT empresa colaborador
+              </label>
+              <input
+                type="text"
+                name="empresa_colaborador"
+                id="empresa_colaborador"
+                placeholder="Escriba el nit de la empresa del colaborador"
+                onChange={onChange}
+              />{" "}
+              <br />
+              <label htmlFor="nombre_1">Primer Nombre</label>
+              <input
+                type="text"
+                name="nombre_1"
+                id="nombre_1"
+                placeholder="Escriba el primer nombre del colaborador"
+                onChange={onChange}
+              />{" "}
+              <br />
+              <label htmlFor="apellido_1">Primer Apellido</label>
+              <input
+                type="text"
+                name="apellido_1"
+                id="apellido_1"
+                placeholder="Escriba el primer apellido del colaborador"
+                onChange={onChange}
+              />{" "}
+              <br />
+              <label htmlFor="genero">Genero</label>
+              <select onChange={onChange} id="genero" name ="genero"aria-label="Seleccione el genero">
+                <option selected>Seleccione el genero</option>
+                <option value="Masculino">Masculino</option>
+                <option value="Femenino">Femenino</option>
+              </select>{" "}
+              
+              
+              <br />
+              <label htmlFor="correo_personal">Correo personal</label>
+              <input
+                type="text"
+                name="correo_personal"
+                id="correo_personal"
+                placeholder="Escriba el correo personal del colaborador"
+                onChange={onChange}
+              />{" "}
+              <br />
+              <label htmlFor="fecha_nacimiento">Fecha nacimiento</label>
+              <input
+                type="datetime-local"
+                name="fecha_nacimiento"
+                id="fecha_nacimiento"
+                placeholder="Ingrese la fecha de nacimiento del colaborador"
+                onChange={onChange}
+              />{" "}
+              <br />
+              <label htmlFor="foto">Foto</label>
+              <input
+                type="file"
+                name="foto"
+                id="foto"
+                placeholder="Escriba el nit de la empresa del colaborador"
+                onChange={onChange}
+              />{" "}
+              <br />
+              <button className="btn btn-primary mt-3">
+                Crear colaborador
+              </button>
+            </form>
+          </main>
+        </div>
+        <div className="col col-lg-2"></div>
+      </div>
     </>
   );
 };
-
-    
-    
-    
-  
