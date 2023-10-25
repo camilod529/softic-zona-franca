bcc2f730673f5f5d9ee47236a804c7cfad4baa01
DFDev3 <ddiaz216@unab.edu.co>
2023-10-25 16:17:20 -0500

    AdmPrize y createPrize

---
+++ src/components/AdmPrizeCard.tsx
@@ -0,0 +1,23 @@
+export const AdmPrizeCard = () => {
+    return (
+      <>
+        <div className="col">
+          <div className="card prizeCard">
+            <img
+              src="https://img.freepik.com/vector-premium/trofeo-oro-placa-ganador-concurso_68708-545.jpg"
+              className="card-img-top"
+              alt="..."
+            />
+            <div className="card-body">
+              <h5 className="card-title">Nombre del premio</h5>
+              <p className="card-text">Mini descripción junto con precio</p>
+              <p className="card-text">Precio</p>
+              <a href="#" className="btn green-btn">
+                Eliminar
+              </a>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  };
--- src/pages/CreateEventPage.tsx
+++ src/pages/CreateEventPage.tsx
@@ -1,13 +1,15 @@
 import { FormEvent, useState } from "react";
 import { useNavigate } from "react-router-dom";
-{/*import { createEvent } from "../api/session";*/}
+{
+  /*import { createEvent } from "../api/session";*/
+}
 import { Navbar } from "../components";
 import Footer from "../components/Footer";
 
 export const CreateEventPage = () => {
   const [data, setData] = useState({
     id_evento: 0,
-    nombre_evento: "",
+    ID_evento: "",
     descripcion_evento: "",
     foto_evento: null,
     fecha_evento: new Date(),
@@ -24,12 +26,13 @@ export const CreateEventPage = () => {
     setData({ ...data, [e.currentTarget.name]: e.currentTarget.value });
   };
 
-  {/*const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+  {
+    /*const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log(data);
     await createEvent({
       id_evento: data.id_evento,
-      nombre_evento: data.nombre_evento,
+      ID_evento: data.ID_evento,
       descripcion_evento: data.descripcion_evento,
       foto_evento: data.foto_evento,
       fecha_evento: data.fecha_evento,
@@ -44,11 +47,12 @@ export const CreateEventPage = () => {
         navigate("/adminEventos");
       })
       .catch((err) => console.log(err));
-  };*/}
+  };*/
+  }
   return (
     <main>
       <Navbar />
-      
+
       <img
         src="https://i.imgur.com/9PDGhjx.jpg"
         className="contenido-alternativa img-fluid contenido "
@@ -58,110 +62,107 @@ export const CreateEventPage = () => {
         <div className="col-md-auto">
           <main>
             <form className="colaborator-form mr-4" /*onSubmit={handleSubmit}*/>
-              <label htmlFor="documento_colaborador">
-                ID Evento
-              </label>
-              <input
-                    type="text"
-                    name="nombre_evento"
-                    id="nombre_evento"
-                    placeholder="Escriba el nombre del evento"
-                    onChange={onChange}
-                  />{" "}
-                  <br />
-              <label htmlFor="empresa_colaborador">
-                Nombre del Evento
-              </label>
+              <label htmlFor="documento_colaborador">ID Evento</label>
               <input
                 type="text"
-                name="empresa_colaborador"
-                id="empresa_colaborador"
-                placeholder="Escriba el nit de la empresa del colaborador"
+                name="ID_evento"
+                id="ID_evento"
+                placeholder="Escriba el ID del evento"
+                onChange={onChange}
+              />{" "}
+              <br />
+              <label htmlFor="nombre_evento">Nombre del Evento</label>
+              <input
+                type="text"
+                name="nombre_evento"
+                id="nombre_evento"
+                placeholder="Escriba nombre del evento"
                 onChange={onChange}
               />{" "}
               <br />
               <label htmlFor="nombre_1">Descripcion del Evento</label>
               <input
-                    type="text"
-                    name="descripcion_evento"
-                    id="descripcion_evento"
-                    placeholder="Escriba la descripcion"
-                    onChange={onChange}
-                  />{" "}
-                  <br />
+                type="text"
+                name="descripcion_evento"
+                id="descripcion_evento"
+                placeholder="Escriba la descripcion"
+                onChange={onChange}
+              />{" "}
+              <br />
               <label htmlFor="apellido_1">Banner</label>
               <input
-                    type="file"
-                    name="foto_evento"
-                    id="foto_evento"
-                    placeholder="Ingrese la foto del eventos"
-                    onChange={onChange}
-                  />{" "}
-
+                type="file"
+                name="foto_evento"
+                id="foto_evento"
+                placeholder="Ingrese la foto del eventos"
+                onChange={onChange}
+              />{" "}
+              <br />
               <br />
               <label htmlFor="genero">Fecha de inicio</label>
               <input
-                    type="datetime-local"
-                    name="fecha_evento"
-                    id="fecha_evento"
-                    placeholder="Ingrese la fecha del evento"
-                    onChange={onChange}
-                  />{" "}
-                  <br />
-              
-              
+                type="datetime-local"
+                name="fecha_evento"
+                id="fecha_evento"
+                placeholder="Ingrese la fecha del evento"
+                onChange={onChange}
+              />{" "}
+              <br />
               <br />
               <label htmlFor="correo_personal">Fecha de cierre</label>
               <input
-                    type="datetime-local"
-                    name="fecha_evento_fin"
-                    id="fecha_evento_fin"
-                    placeholder="Ingrese la fecha final del evento"
-                    onChange={onChange}
-                  />{" "}
-                  <br />
+                type="datetime-local"
+                name="fecha_evento_fin"
+                id="fecha_evento_fin"
+                placeholder="Ingrese la fecha final del evento"
+                onChange={onChange}
+              />{" "}
+              <br />
               <br />
               <label htmlFor="fecha_nacimiento">Aforo Maximo</label>
               <input
-                    min={1}
-                    type="number"
-                    name="aforo_maximo"
-                    id="aforo_maximo"
-                    placeholder="aforo maximo"
-                    onChange={onChange}
-                  />{" "}
-                  <br />
+                min={1}
+                type="number"
+                name="aforo_maximo"
+                id="aforo_maximo"
+                placeholder="Aforo maximo"
+                onChange={onChange}
+              />{" "}
+              <br />
               <label htmlFor="foto">Aforo registrado</label>
               <input
-                    min={0}
-                    type="number"
-                    name="aforo_registrado"
-                    id="aforo_registrado"
-                    placeholder="aforo registrado"
-                    onChange={onChange}
-                  />{" "}
-                  <br />
-                  <label htmlFor="foto">Puntos para Colaboradores</label>
-                  <input
-                    min={0}
-                    type="number"
-                    name="puntos_colaborador"
-                    id="puntos_colaborador"
-                    placeholder="Ingrese los puntos a asignar al colaborador por asistir al evento"
-                    onChange={onChange}
-                  />{" "}
-                  <br />
-                  <label htmlFor="foto">Puntos a Empresas</label>
-                  <input
-                    min={0}
-                    type="number"
-                    name="puntos_empresa"
-                    id="puntos_empresa"
-                    placeholder="Ingrese los puntos a asignar a la empresa"
-                    onChange={onChange}
-                  />{" "}
-              <button className="btn btn-primary mt-3">
-                Crear colaborador
+                min={0}
+                type="number"
+                name="aforo_registrado"
+                id="aforo_registrado"
+                placeholder="Aforo registrado"
+                onChange={onChange}
+              />{" "}
+              <br />
+              <label htmlFor="foto">Puntos para Colaboradores</label>
+              <input
+                min={0}
+                type="number"
+                name="puntos_colaborador"
+                id="puntos_colaborador"
+                placeholder="Ingrese los puntos a asignar al colaborador por asistir al evento"
+                onChange={onChange}
+              />{" "}
+              <br />
+              <label htmlFor="foto">Puntos a Empresas</label>
+              <input
+                min={0}
+                type="number"
+                name="puntos_empresa"
+                id="puntos_empresa"
+                placeholder="Ingrese los puntos a asignar a la empresa"
+                onChange={onChange}
+              />{" "}
+              <button
+                className="btn btn-primary mt-3 green-btn"
+                style={{ width: "30%" }}
+              >
+                Crear evento
               </button>
             </form>
           </main>
