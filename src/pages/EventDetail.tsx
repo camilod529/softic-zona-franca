fd218fa4c70b708ebb7755fdc89c6e2519869e64
DFDev3 <104524791+DFDev3@users.noreply.github.com>
2023-10-26 11:44:08 -0500

    Añadir encabezados faltantes

--- src/pages/EventDetail.tsx
+++ src/pages/EventDetail.tsx
@@ -31,15 +31,18 @@ export function EventDetail() {
 
   return (
     <>
-
       <Navbar />
+      <img
+        src="https://www.zonafrancasantander.com/imagenes//vco_secciones/secc_37d4bbfbf2565771790085cdc4767c80413274bd.jpg"
+        className="img-fluid banner"
+      />
       <div className="">
         <div className="container titulo">
           {event.id_evento !== 0 ? (
             <>
-             <h1 className="text-center mt-5 mb-5 display-2">
-                    {event.nombre_evento}
-                  </h1>
+              <h1 className="text-center mt-5 mb-5 display-2">
+                {event.nombre_evento}
+              </h1>
               <div className="row">
                 <div className="col m-3">
                   <img
@@ -49,37 +52,40 @@ export function EventDetail() {
                   />
                 </div>
                 <div className="col">
-                 
                   <h3 className="m-3">Fecha:</h3>
-                  <h5 className="m-3">{new Date(event.fecha_evento).toDateString()}</h5>
+                  <h5 className="m-3">
+                    {new Date(event.fecha_evento).toDateString()}
+                  </h5>
                   <h3 className="m-3">Descripcion:</h3>
                   <h5 className="m-3">{event.descripcion_evento}</h5>
                   <h3 className="m-3">Etiquetas:</h3>
                 </div>
               </div>
               <div className="container text-center">
-  <div className="row justify-content-md-center m-5">
-    <div className="col col-lg-2">
-      
-    </div>
-    <div className="col-md-auto">
-    <form>
-  <fieldset className="text-center">
-    <h1> Pre-registrate aqui</h1>
-    <div className="mb-3 formSpace">
-      <label className="form-label"></label>
-      <input type="text"  className="form-control " placeholder="Cedula"/>
-    </div>
+                <div className="row justify-content-md-center m-5">
+                  <div className="col col-lg-2"></div>
+                  <div className="col-md-auto">
+                    <form>
+                      <fieldset className="text-center">
+                        <h1> Pre-registrate aqui</h1>
+                        <div className="mb-3 formSpace">
+                          <label className="form-label"></label>
+                          <input
+                            type="text"
+                            className="form-control "
+                            placeholder="Cedula"
+                          />
+                        </div>
 
-    <button type="submit" className="btn btn-primary">Submit</button>
-  </fieldset>
-</form>
-    </div>
-    <div className="col col-lg-2">
-      
-    </div>
-  </div>
-  </div>
+                        <button type="submit" className="btn btn-primary">
+                          Submit
+                        </button>
+                      </fieldset>
+                    </form>
+                  </div>
+                  <div className="col col-lg-2"></div>
+                </div>
+              </div>
             </>
           ) : (
             <h1>No existe el evento</h1>
